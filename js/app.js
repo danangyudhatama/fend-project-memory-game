7b99030968a788970e16a671053ee36469a840f2
danangyudhatama <danang.yudhatama@gmail.com>
2017-10-02 03:01:35 +0200

    first commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+
+
+
+
+
 /*
  * Create a list that holds all of your cards
  */
-
+var cards = ["fa-camera-retro","fa-camera-retro","fa-car","fa-car","fa-anchor","fa-anchor","fa-bomb","fa-bomb","fa-diamond","fa-diamond","fa-heart","fa-heart","fa-lightbulb-o","fa-lightbulb-o","fa-plane","fa-plane"];
 
 /*
  * Display the cards on the page
@@ -9,6 +14,17 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+shuffle(cards);
+
+for (var i = cards.length - 1; i >= 0; i--) {
+    $(".deck").append(`<li class="card">
+                <i class="fa `+cards[i]+`"></i>
+            </li>`);
+ };
+
+
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,7 +41,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +51,59 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+var move = 1;
+var openedCard = [];    //placeholder for card that have been opened
+
+$("li").on("click",function(){  //listen to card that being clicked
+        openedCard.push($(this));
+        $(this).addClass("show");   //the card will be opened and showed
+        $(this).addClass("open");
+        compareCard();
+        playerWin();
+})
+
+
+
+function incrementMove() {
+    $(".moves").html(move);
+    move++;
+}
+
+function compareCard() {
+    if (openedCard.length === 2){   //comparing if the array filled with 2 cards   //after the card open, push the card to the array 
+        incrementMove();
+        if (openedCard[0].children().attr("class") === openedCard[1].children().attr("class")) {    //if cards are the same then add the class matched to the cards, after that empty the cards holder array
+            matchedCard();
+            openedCard =[];
+        } 
+        else {      //else the card will be closed again and empty the cards holder array 
+            doNotMatchedCard();
+            openedCard =[];
+        }
+    }  
+}
+
+function matchedCard() {
+    openedCard[0].addClass("matched");
+    openedCard[1].addClass("matched");
+}
+
+function doNotMatchedCard() {
+    openedCard[0].removeClass("open show");
+    openedCard[1].removeClass("open show");
+}
+
+
+$(".restart").click(function() {
+    location.reload();
+}) 
+
+function playerWin() {
+    if ($(".deck").children().length == $(".deck").children(".matched").length) {
+        alert("wooo you win the game !!!");
+        location.reload();
+    }
+
+}
